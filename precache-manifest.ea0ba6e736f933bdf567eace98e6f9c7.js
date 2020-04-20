6b40330fffa4992c07d1ca35eefbc7f99207a838
Naoki Mita <naoki.mita18@gmail.com>
2020-04-19 22:02:36 -0700

    Updates

---
+++ precache-manifest.ea0ba6e736f933bdf567eace98e6f9c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcee14c4d9b24fbcd3f6419a420183c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "169c24fb91c82d95d76e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7f141faa93254d62ba5a",
+    "url": "/static/js/2.d90ae76b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d90ae76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "169c24fb91c82d95d76e",
+    "url": "/static/js/main.0291b100.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
