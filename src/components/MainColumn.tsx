89f6a085e6f04b58c368708bd82b4429b06d1a7b
Naoki Mita <naoki.mita18@gmail.com>
2020-04-23 22:28:36 -0700

    Add about-me section

--- src/components/MainColumn.tsx
+++ src/components/MainColumn.tsx
@@ -4,7 +4,19 @@ import "./MainColumn.scss";
 const MainColumn: React.FC = () => {
   return (
     <article className='main-column'>
-      <section>aaaaaaa</section>
+      <section className='about-me'>
+        <div className='about-me-description'>
+          <h1>Naoki Mita (美田 直毅)</h1>
+          <h2>Software Developer / Data Analyst</h2>
+          <p>
+            I am a software developer specialised in frontend and backend development for web apps. Also, I have experimence of being a data analyst.
+            I believe I can help you to grow up your products in every aspects. Check out my portfolio and online resume.
+          </p>
+        </div>
+        <div className='about-me-image'>
+          <img src='/portfolio/img/about_me.jpg' alt='' />
+        </div>
+      </section>
     </article>
   );
 };
