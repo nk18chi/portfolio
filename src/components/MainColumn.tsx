bb3e81fbea5147cfe7f2b18451594b48387613eb
Naoki Mita <naoki.mita18@gmail.com>
2020-04-24 15:31:28 -0700

    Add skill-sets section

--- src/components/MainColumn.tsx
+++ src/components/MainColumn.tsx
@@ -2,12 +2,39 @@ import React from "react";
 import "./MainColumn.scss";
 
 const MainColumn: React.FC = () => {
+  let skillSets = [
+    {
+      title: "Typescript & React",
+      description:
+        "The mission of the Python Software Foundation is to promote, protect, and advance the Python programming language, and to support and facilitate the growth of a diverse and international community of Python programmers.",
+      icons: ["/portfolio/img/typescript.svg", "/portfolio/img/react.svg"],
+    },
+    {
+      title: "Python & Django",
+      description:
+        "The mission of the Python Software Foundation is to promote, protect, and advance the Python programming language, and to support and facilitate the growth of a diverse and international community of Python programmers.",
+      icons: ["/portfolio/img/python.svg", "/portfolio/img/django.svg"],
+    },
+    {
+      title: "Data Analysis",
+      description:
+        "The mission of the Python Software Foundation is to promote, protect, and advance the Python programming language, and to support and facilitate the growth of a diverse and international community of Python programmers.",
+      icons: ["/portfolio/img/google-analytics.svg", "/portfolio/img/google-big-query.png", "/portfolio/img/sql.svg"],
+    },
+    {
+      title: "Mobile Apps",
+      description:
+        "The mission of the Python Software Foundation is to promote, protect, and advance the Python programming language, and to support and facilitate the growth of a diverse and international community of Python programmers.",
+      icons: ["/portfolio/img/apple.svg", "/portfolio/img/android.svg"],
+    },
+  ];
+
   return (
     <article className='main-column'>
       <section className='about-me block'>
         <div className='about-me-description'>
           <h1>Naoki Mita (美田 直毅)</h1>
-          <h2>Software Developer / Data Analyst</h2>
+          <h3>Software Developer / Data Analyst</h3>
           <p>
             I am a software developer specialised in frontend and backend development for web apps. Also, I have experimence of being a data analyst.
             I believe I can help you to grow up your products in every aspects. Check out my portfolio and online resume.
@@ -16,6 +43,33 @@ const MainColumn: React.FC = () => {
         <div className='about-me-image'>
           <img src='/portfolio/img/about_me.jpg' alt='' />
         </div>
+      </section>
+
+      <section className='skill-sets block'>
+        <h2>What I do</h2>
+        <p>
+          I have experiance as a software developer and a data analyst for clients in Japan. Below is a quick overview of my technical skill sets and
+          technologies I use. Check out my portfolio as well.
+        </p>
+        <ul>
+          {skillSets.length > 0 &&
+            skillSets.map((skill: any) => (
+              <li className='skill-each-set'>
+                <div className='icon'>
+                  <ul>
+                    {skill.icons.length > 0 &&
+                      skill.icons.map((icon: any) => (
+                        <li>
+                          <img src={icon} alt='' />
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+                <h3>{skill.title}</h3>
+                <p>{skill.description}</p>
+              </li>
+            ))}
+        </ul>
       </section>
     </article>
   );
