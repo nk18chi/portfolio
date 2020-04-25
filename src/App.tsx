10b88de7d77b3e96108bdf2d9f9271ef7fd3b4c4
Naoki Mita <naoki.mita18@gmail.com>
2020-04-25 15:10:34 -0700

    Can move to portfolio page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./index.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SideBar from "./components/SideBar/SideBar";
-import MainColumn from "./components/TopPage/MainColumn";
+import TopPage from "./components/TopPage/TopPage";
+import PortfolioPage from "./components/PortfolioPage/PortfolioPage";
 
 function App() {
   return (
     <>
       <div className='container'>
         <SideBar />
-        <MainColumn />
+        <Router>
+          <Switch>
+            <Route exact path='/portfolio/p_:portfolio_id' component={PortfolioPage} />
+            <Route exact path='/portfolio' component={TopPage} />
+          </Switch>
+        </Router>
       </div>
       <footer>2020 © Naoki Mita. All Rights Reserved.</footer>
     </>
