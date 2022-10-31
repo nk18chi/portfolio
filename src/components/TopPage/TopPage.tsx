8c37db61d9fdea1b26c34d94e36b98f1c18f8459
Naoki Mita <naoki.mita18@gmail.com>
2022-10-31 14:31:50 -0700

    feat: add values/favorites

--- src/components/TopPage/TopPage.tsx
+++ src/components/TopPage/TopPage.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./TopPage.scss";
 import { Button } from "@material-ui/core";
-import { Favorite, FindReplace, Group, GitHub, LinkedIn, Twitter, Description } from "@material-ui/icons";
+import { Build, Group, GitHub, LinkedIn, Twitter, Description, RateReview, DoneAll } from "@material-ui/icons";
 import LinkIcon from "@material-ui/icons/Link";
 import { privateProjectData } from "../../data/privateProject";
 import IconButtonLink from "./IconButtonLink";
 import { EXPERIENCES } from "../../data/experiences";
 import { calcDiffDate, formatDate } from "../../utils/common";
-
 const iconData = [
   { url: "https://github.com/nk18chi", element: <GitHub /> },
   { url: "https://www.linkedin.com/in/naoki-mita-ab186839/", element: <LinkedIn /> },
@@ -48,22 +47,75 @@ const skillSets: TSkillset[] = [
   { label: "Salesforce Commerce API", icon: "/portfolio/img/icon/salesforce.svg" },
 ];
 
+type TFavarites = {
+  title: string;
+  description: string[];
+  link: string;
+};
+const favorites: TFavarites[] = [
+  {
+    title: "A Philosophy of Software Design",
+    description: [
+      "Cause of complexity is two. Dependencies and obscurity",
+      "The ideal design tends to emerge in bits and pieces. Thus, the best approach is to make lots of small investments on continues basis",
+      "Interfaces should be desinged to make the common case as simple as possible. Interface should be less and implementation should be bigger.",
+    ],
+    link: "https://www.amazon.ca/Philosophy-Software-Design-John-Ousterhout/dp/1732102201",
+  },
+  {
+    title: "Clean Code",
+    description: [
+      "It is a myth that we can get systems “right the first time.” Instead, we should implement only today’s stories, then refactor and expand the system to implement new stories tomorrow. This is the essence of iterative and incremental agility. Test-driven development, refactoring, and the clean code they produce make this work at the code level.",
+      "Systems must be clean too.An invasive architecture overwhelms the domain logic and impacts agility.When the domain logic is obscured, quality suffers because bugs find it easier to hide and stories become harder to implement.If agility is compromised, productivity suffers and the benefits of TDD are lost.",
+      "Remarkably, following a simple and obvious rule that says we need to have tests and run them continuously impacts our system’s adherence to the primary OO goals of low coupling and high cohesion. Writing tests leads to better designs.",
+      "All too often we get our code working and then move on to the next problem without giving sufficient thought to making that code easy for the next person to read. Remember, the most likely next person to read the code will be you. So take a little pride in your workmanship. Spend a little time with each of your functions and classes. Choose better names, split large functions into smaller functions, and generally just take care of what you’ve created. Care is a precious resource.",
+    ],
+    link: "https://www.amazon.ca/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882/",
+  },
+  {
+    title: "Small Functions considered Harmful",
+    description: [
+      "“duplication is far cheaper than the wrong abstraction”, and thus to “prefer duplication over the wrong abstraction”.",
+      "The problem with “small functions” though, is that the quest for small functions ends up begetting even more small functions, all of which tend to be given extremely verbose names in the spirit of making code self documenting and eschewing comments.",
+      "Hard to Read: This has already been stated before but it bears reiterating — an explosion of small functions, especially one line functions, makes the codebase inordinately harder to read. This especially hurts those for whom the code should’ve been optimized for in the first place — newcomers.",
+    ],
+    link: "https://copyconstruct.medium.com/small-functions-considered-harmful-91035d316c29",
+  },
+  {
+    title: "Write Code Every Day",
+    description: [
+      "Writing code every day makes lots of benefits as a developer.",
+      "Code as habit. This is about a change that you’re making in your life for yourself not a change that you’re making to satisfy someone else’s perception of your work.",
+      "Background processing. Your current task is frequently running in the back of your mind when I go for a walk, or take a shower, or any of the other non-brain-using activities I participate in.",
+    ],
+    link: "https://johnresig.com/blog/write-code-every-day",
+  },
+];
+
 const myValues = [
   {
-    icon: <Favorite />,
-    title: "Passion for building apps",
-    description: "I love building apps and thinking about how make users comfortable. I never stop learning and building the app even free time.",
-  },
-  {
-    icon: <FindReplace />,
-    title: "Think efficiently",
-    description: "I always keep looking for the way to reduce time. I love automation and effective-algorithm.",
-  },
-  {
-    icon: <Group />,
-    title: "Work together well",
+    icon: <Build className='color-gray' />,
+    title: "Passion for making code clean",
     description:
-      "I can smoothly work with a team from other departments because I have knowledge of not only building apps but also analysis and marketing, design etc.",
+      "As a developer, you must spend much more time to read code than write it. So, I believe it is more important to think of how other developers will understand my code than how I like to construct it.",
+  },
+  {
+    icon: <DoneAll className='color-green' />,
+    title: "Test believer",
+    description:
+      "Having test code makes the code understandable, and other developers are not afraid of changing your code without breaking existing functionalities.",
+  },
+  {
+    icon: <RateReview className='color-lightblue' />,
+    title: "Love to review code and be reviewed",
+    description:
+      "I would like to know how other developers think of my code and how they write code. It is a great chance of learning new from others!",
+  },
+  {
+    icon: <Group className='color-lightseagreen' />,
+    title: "Working as team takes you go further",
+    description:
+      "I believe you can achieve more and get your goal faster than you do alone. The great communication is built by the atmosphere that everyone can feel free to ask anything with good manners.",
   },
 ];
 
@@ -115,7 +167,7 @@ const TopPage: React.FC = () => {
               </div>
               <p className='company-name'>{exp.company.name}</p>
               <p>{exp.company.description}</p>
-              <ul className='job-contents'>
+              <ul className='list'>
                 {exp.job.contents.map((c) => (
                   <li key={c}>{c}</li>
                 ))}
@@ -141,16 +193,39 @@ const TopPage: React.FC = () => {
 
       <section id='my-values' className='block'>
         <h2>My Values</h2>
-        <ul>
+        <div className='stack'>
           {myValues.length > 0 &&
             myValues.map((value: any, index: number) => (
-              <li key={index} className='three-elements'>
-                <p className='center app-big-icon'>{value.icon}</p>
-                <h3 className='center'>{value.title}</h3>
+              <div key={index} className='vstack w-50'>
+                <div className='hstack'>
+                  <span>{value.icon}</span>
+                  <h3>{value.title}</h3>
+                </div>
                 <p>{value.description}</p>
-              </li>
+              </div>
             ))}
-        </ul>
+        </div>
+      </section>
+
+      <section id='my-favorites' className='block'>
+        <h2>Faves</h2>
+        <div id='my-favorites-container'>
+          {favorites.length > 0 &&
+            favorites.map((favorite, i: number) => (
+              <div key={i} className='my-favorite'>
+                <h3>
+                  <a href={favorite.link} target='_blank_'>
+                    {favorite.title}
+                  </a>
+                </h3>
+                <ul className='list'>
+                  {favorite.description.map((desc, j) => (
+                    <li key={j}>{desc}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+        </div>
       </section>
 
       <section id='privateProjects' className='privateProjects block'>
